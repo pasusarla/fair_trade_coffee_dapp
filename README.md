e6dffed941b6c28ea3f4a375bbdeb6cdb7e7bc1a
Prashanth Aditya Susarla <prashanth.susarla@gmail.com>
2020-10-09 15:14:07 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# fair_trade_coffee_dapp
+# Fair Trade Coffee DApp
+
+This distributed app on Ethereum implements a very simple supply chain for a coffee product.
+
+Details on -
+
+* the attributes of this product
+* each of the actors in the supply chain and their roles and responsibilities, and
+* the state of the supply chain
+
+can be found in the UML diagrams inside the uml/ folder under the project root.
+
+## Versions of various packages
+```
+$ truffle version
+Truffle v5.1.47 (core: 5.1.47)
+Solidity - 0.6.12 (solc-js)
+Node v14.12.0
+Web3.js v1.2.1
+```
+
+```
+$ npm ls truffle-hdwallet-provider lite-server web3
+fair-trade-coffee-dapp@1.0.0 
++-- lite-server@2.4.0
+`-- truffle-hdwallet-provider@1.0.17
+  `-- web3@1.2.1
+```
+truffle-hdwallet-provider is used to run the truffle test suite on the public test network (Ropsten, in this case). This library can be initialized to allow the calling code to access just the required number of accounts (5, in this case) given the seed phrase.
+lite-server is used to run the client-facing web app.
+web3 is self-explanatory.
+
+## Token Identification
+Ropsten contract address: https://ropsten.etherscan.io/address/0x80821b4e459f5fc5de501198827a039dd743a916
+Ropsten contract creation transaction: https://ropsten.etherscan.io/tx/0x7505bbfa39d17c7414d24f751b6b173a3f6861cf2f87a51af6be4b55685babee
+
+Entire lifecycle for UPC 2:
+Harvested - https://ropsten.etherscan.io/tx/0x92f16ea4d86b0db035a909482241367cd06d59711a96dca4f19019c8c87c502a
+Processed - https://ropsten.etherscan.io/tx/0x3ea5b9640833c92c2256911ff76a4aa27df79aa9b2b6f0670990739b994cd461
+Packed - https://ropsten.etherscan.io/tx/0x15dade0520c2ffda3d41e7e48a067aa33222df6982cc3ca2c4a97b0c2a29c2d9
+ForSale - https://ropsten.etherscan.io/tx/0x1cbcbb4835edbe86d8c1c599d6703d0bb85b64cd530ee418537c482c2e9c00a6
+Sold - https://ropsten.etherscan.io/tx/0x4538abd1c75864a953069564a7e4a831ffd359cce22357d9e99a4f6d5a596409
+Shipped - https://ropsten.etherscan.io/tx/0x9736b737937e95cbc58a834696035f6d27b626ac26bb90e44d4a62c1853477cc
+Received - https://ropsten.etherscan.io/tx/0xa6b15fa39a6c28b860a4ed5a08faffe4bb832251bbecc1bf5d6ae0f3d05ab1b8
+Purchased - https://ropsten.etherscan.io/tx/0x3343f4df3436aac83a867695b54f0a20949fe7fb694a4a9b4da1c9bff71e7c20
---
+++ src/js/app.js
@@ -0,0 +1,342 @@
+App = {
+    web3Provider: null,
+    contracts: {},
+    emptyAddress: "0x0000000000000000000000000000000000000000",
+    upc: 0,
+    metamaskAccountID: "0x0000000000000000000000000000000000000000",
+    ownerID: "0x0000000000000000000000000000000000000000",
+    originFarmerID: "0x0000000000000000000000000000000000000000",
+    originFarmName: null,
+    originFarmInformation: null,
+    originFarmLatitude: null,
+    originFarmLongitude: null,
+    productNotes: null,
+    productPrice: 0,
+    distributorID: "0x0000000000000000000000000000000000000000",
+    retailerID: "0x0000000000000000000000000000000000000000",
+    consumerID: "0x0000000000000000000000000000000000000000",
+
+    init: async function() {
+        App.readForm();
+        /// Setup access to blockchain
+        return await App.initWeb3();
+    },
+
+    readForm: function() {
+        App.upc = $("#upc").val();
+        App.originFarmName = $("#originFarmName").val();
+        App.originFarmInformation = $("#originFarmInformation").val();
+        App.originFarmLatitude = $("#originFarmLatitude").val();
+        App.originFarmLongitude = $("#originFarmLongitude").val();
+        App.productNotes = $("#productNotes").val();
+        App.productPrice = $("#productPrice").val();
+    },
+
+    initWeb3: async function() {
+        function handleAccountsChanged(accounts) {
+            if (accounts.length === 0) {
+                // MetaMask is locked or the user has not connected any accounts
+                console.log('Please connect to MetaMask.');
+            } else if (accounts[0] !== App.metamaskAccountID) {
+                // Retrieving accounts
+                App.metamaskAccountID = accounts[0];
+                if (App.ownerID === App.emptyAddress) {
+                    App.ownerID = accounts[0];
+                    web3.eth.defaultAccount = accounts[0];
+                }
+            }
+        }
+
+        App.web3Provider = window.ethereum;
+        var accounts;
+        
+        if (App.web3Provider) {
+            // From now on, this should always be true:
+            // provider === window.ethereum
+            if (App.web3Provider !== window.ethereum) {
+                console.error('Do you have multiple wallets installed?');
+            }
+        } else {
+          console.log('Please install MetaMask!');
+        }
+
+        let currentChainId = ethereum.chainId;
+        ethereum.on('chainChanged', function(_chainId) {
+            window.location.reload();
+        });
+
+        /// Find or Inject Web3 Provider
+        /// Modern dapp browsers...
+        if (App.web3Provider) {
+            // Request account access
+            accounts = ethereum.request({method: 'eth_accounts'})
+                               .then(handleAccountsChanged)
+                               .catch((err) => {
+                                   console.error(err);
+                               });
+            ethereum.on('accountsChanged', handleAccountsChanged);
+        }
+        // Legacy dapp browsers...
+        else if (window.web3) {
+            App.web3Provider = window.web3.currentProvider;
+            accounts = web3.eth.accounts;
+        }
+        // If no injected web3 instance is detected, fall back to Ganache
+        else {
+            App.web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:7545');
+            web3 = new Web3(App.web3Provider);
+            accounts = web3.eth.accounts;
+        }
+
+        return App.initSupplyChain();
+    },
+
+    initSupplyChain: function() {
+        /// Source the truffle compiled smart contracts
+        var jsonSupplyChain='../../build/contracts/SupplyChain.json';
+        
+        /// JSONfy the smart contracts
+        $.getJSON(jsonSupplyChain, function(data) {
+            var SupplyChainArtifact = data;
+            App.contracts.SupplyChain = TruffleContract(SupplyChainArtifact);
+            App.contracts.SupplyChain.setProvider(App.web3Provider);
+            
+            App.fetchItemBufferOne();
+            App.fetchItemBufferTwo();
+            App.fetchEvents();
+        });
+
+        return App.bindEvents();
+    },
+
+    bindEvents: function() {
+        $(document).on('click', App.handleButtonClick);
+    },
+
+    handleButtonClick: async function(event) {
+        event.preventDefault();
+
+        var processId = parseInt($(event.target).data('id'));
+
+        App.upc = $("#upc").val();
+        switch (processId) {
+            case 1:
+                await App.harvestItem(event);
+                break;
+            case 2:
+                await App.processItem(event);
+                break;
+            case 3:
+                await App.packItem(event);
+                break;
+            case 4:
+                await App.sellItem(event);
+                break;
+            case 5:
+                await App.buyItem(event);
+                break;
+            case 6:
+                await App.shipItem(event);
+                break;
+            case 7:
+                await App.receiveItem(event);
+                break;
+            case 8:
+                await App.purchaseItem(event);
+                break;
+            default:
+                break;
+            }
+
+            await App.fetchItemBufferOne(event);
+            await App.fetchItemBufferTwo(event);
+            return;
+    },
+
+    harvestItem: function(event) {
+        event.preventDefault();
+
+        App.contracts.SupplyChain.deployed().then(function(instance) {
+            App.originFarmerID = App.metamaskAccountID;
+            $("#farmerID").html(App.originFarmerID);
+
+            return instance.harvestItem(
+                App.upc, 
+                App.metamaskAccountID, 
+                App.originFarmName, 
+                App.originFarmInformation, 
+                App.originFarmLatitude, 
+                App.originFarmLongitude, 
+                App.productNotes,
+                {from: App.metamaskAccountID}
+            );
+        }).then(function(result) {
+            $("#ftc-item").text(result);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    processItem: function (event) {
+        event.preventDefault();
+
+        App.contracts.SupplyChain.deployed().then(function(instance) {
+            App.originFarmerID = App.metamaskAccountID;
+            $("#farmerID").html(App.originFarmerID);
+
+            return instance.processItem(App.upc, {from: App.metamaskAccountID});
+        }).then(function(result) {
+            $("#ftc-item").text(result);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+    
+    packItem: function (event) {
+        event.preventDefault();
+
+        App.contracts.SupplyChain.deployed().then(function(instance) {
+            App.originFarmerID = App.metamaskAccountID;
+            $("#farmerID").html(App.originFarmerID);
+
+            return instance.packItem(App.upc, {from: App.metamaskAccountID});
+        }).then(function(result) {
+            $("#ftc-item").text(result);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    sellItem: function (event) {
+        event.preventDefault();
+
+        App.contracts.SupplyChain.deployed().then(function(instance) {
+            App.originFarmerID = App.metamaskAccountID;
+            $("#farmerID").html(App.originFarmerID);
+
+            return instance.sellItem(App.upc, App.productPrice, {from: App.metamaskAccountID});
+        }).then(function(result) {
+            $("#ftc-item").text(result);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    buyItem: function (event) {
+        event.preventDefault();
+
+        App.contracts.SupplyChain.deployed().then(function(instance) {
+            App.distributorID = App.metamaskAccountID;
+            $("#distributorID").html(App.distributorID);
+            const walletValue = web3.toWei(2000000000, "wei");
+
+            return instance.buyItem(App.upc, {from: App.metamaskAccountID, value: walletValue});
+        }).then(function(result) {
+            $("#ftc-item").text(result);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    shipItem: function (event) {
+        event.preventDefault();
+
+        App.contracts.SupplyChain.deployed().then(function(instance) {
+            App.distributorID = App.metamaskAccountID;
+            $("#distributorID").html(App.distributorID);
+
+            return instance.shipItem(App.upc, {from: App.metamaskAccountID});
+        }).then(function(result) {
+            $("#ftc-item").text(result);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    receiveItem: function (event) {
+        event.preventDefault();
+
+        App.contracts.SupplyChain.deployed().then(function(instance) {
+            App.retailerID = App.metamaskAccountID;
+            $("#retailerID").html(App.retailerID);
+            const walletValue = web3.toWei(2000000000, "wei");
+
+            return instance.receiveItem(App.upc, {from: App.metamaskAccountID, value: walletValue});
+        }).then(function(result) {
+            $("#ftc-item").text(result);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    purchaseItem: function (event) {
+        event.preventDefault();
+
+        App.contracts.SupplyChain.deployed().then(function(instance) {
+            App.consumerID = App.metamaskAccountID;
+            $("#consumerID").html(App.consumerID);
+            const walletValue = web3.toWei(2000000000, "wei");
+
+            return instance.purchaseItem(App.upc, {from: App.metamaskAccountID, value: walletValue});
+        }).then(function(result) {
+            $("#ftc-item").text(result);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    fetchItemBufferOne: function () {
+        App.upc = $('#upc').val();
+
+        App.contracts.SupplyChain.deployed().then(function(instance) {
+            return instance.fetchItemBufferOne(App.upc);
+        }).then(function(result) {
+            App.ownerID = result[2];
+            $("#ownerID").html(App.ownerID);
+            App.originFarmerID = result[3];
+            $("#farmerID").html(App.originFarmerID);
+            $("#ftc-item").text(result);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    fetchItemBufferTwo: function () {
+        App.contracts.SupplyChain.deployed().then(function(instance) { 
+            return instance.fetchItemBufferTwo.call(App.upc);
+        }).then(function(result) {
+            App.distributorID = result[6];
+            $("#distributorID").html(App.distributorID);
+            App.retailerID = result[7];
+            $("#retailerID").html(App.retailerID);
+            App.consumerID = result[8];
+            $("#consumerID").html(App.consumerID);
+            $("#ftc-item").text(result);
+        }).catch(function(err) {
+            console.log(err.message);
+        });
+    },
+
+    fetchEvents: function () {
+        if (typeof App.contracts.SupplyChain.currentProvider.sendAsync !== "function") {
+            App.contracts.SupplyChain.currentProvider.sendAsync = function () {
+                return App.contracts.SupplyChain.currentProvider.send.apply(App.contracts.SupplyChain.currentProvider, arguments);
+            };
+        }
+
+        App.contracts.SupplyChain.deployed().then(function(instance) {
+            var events = instance.allEvents(function(err, log) {
+                if (!err)
+                    $("#ftc-events").append('<li>' + log.event + ' - ' + log.transactionHash + '</li>');
+            });
+        }).catch(function(err) {
+          console.log(err.message);
+        });
+    }
+};
+
+$(function () {
+    $(window).load(function () {
+        App.init();
+    });
+});
